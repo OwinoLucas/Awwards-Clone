68ebd9631f8de59ba4fffec86f3ffb8a028fc913
Lucas Owino <lucowish35@gmail.com>
2020-06-07 16:07:05 +0300

    created sidebar for easier user navigation

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    """
+    class containing projects' objects
+    """
+    project_name = models.CharField(max_length=50, blank=True)
+    project_image = models.ImageField(upload_to='projectimages/', blank=True)
+    description = models.TextField(max_length=300, blank=True)
+    github_repo = models.CharField(max_length=150, blank=True)
+    url = models.CharField(max_length=50, blank=True)
