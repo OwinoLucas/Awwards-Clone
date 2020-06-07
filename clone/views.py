fcf6ccc32b96f6813414699937a38dd5354891ec
Lucas Owino <lucowish35@gmail.com>
2020-06-07 13:50:03 +0300

    created projects and app and installed required dependancies to get the app up and running

---
+++ clone/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+
+# Create your views here.
+def index(request):
+    """
+    view function renders the landing page
+    """
+    return render(request, 'index.html')
