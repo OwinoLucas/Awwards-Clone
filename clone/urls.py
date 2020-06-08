308f879af033c05751a489074cd7a2e852e63be0
Lucas Owino <lucowish35@gmail.com>
2020-06-08 11:11:04 +0300

    put the update form inside a modal structure for better user experience

--- clone/urls.py
+++ clone/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     url(r'profile/', views.profile, name='profile'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^project/(?P<project_id>[0-9])$',views.project,name ='project'),
+    url(r'^post/', views.upload_form, name='post'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- clone/views.py
+++ clone/views.py
@@ -2,20 +2,11 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm,UploadForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile,Projects,Image
 
 # Create your views here.
-@login_required(login_url='login')
-def index(request):
-    """
-    view function renders the landing page
-    """
-    current_user = request.user
-    all_projects = Projects.objects.all()
-    return render(request, 'index.html', {'all_projects':all_projects})
-
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -28,9 +19,20 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'registration/register.html', {'form':form})
 
+
+@login_required(login_url='login')
+def index(request):
+    """
+    view function renders the landing page
+    """
+    current_user = request.user
+    all_projects = Projects.objects.all()
+    return render(request, 'index.html', {'all_projects':all_projects})
+
+
 @login_required(login_url='login')
 def profile(request):
-   
+    current_user = request.user.profile
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST,instance=request.user)
         p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
@@ -42,10 +44,13 @@ def profile(request):
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
+        my_projects = Projects.objects.filter(project_user=current_user)
+        
     
     context = {
         'u_form': u_form,
-        'p_form': p_form    
+        'p_form': p_form,
+        'my_projects':my_projects    
     }
     return render(request, 'profile.html', context)
 
@@ -72,3 +77,16 @@ def project(request, project_id):
         raise Http404()
     return render(request, "project.html", {'project':project})
 
+@login_required(login_url='login')
+def upload_form(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.uploaded_by = current_user
+            image.save()
+            return redirect('index')
+    else:
+        form = UploadForm()
+    return render(request, 'post.html', {'uploadform': form})
